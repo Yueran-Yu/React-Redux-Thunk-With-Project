1ef8fbf3964cd4bc750c9dd42f3452a5fb7c0c22
GraceYu <librayuyueran@gmail.com>
2021-11-18 20:39:52 -0600

    why we need to use redux thunk

--- src/redux/actions/productsActions.js
+++ src/redux/actions/productsActions.js
@@ -1,5 +1,21 @@
 import { ActionTypes } from "../constants/action-types";
+import fakeStoreApi from "../../apis/fakeStoreApi";
 
+
+/*
+when you actually make an async api call in your action creator,
+you need to convert your action creator from a synchronous action creator to an asynchronous action creator.
+This conversion can be possible if we use a middleware and that is where we are going to use redux thunk.
+ */
+export const fetchProducts = async () => {
+
+  const response = await fakeStoreApi.get('/products')
+  console.log(response)
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: response,
+  };
+};
 export const setProducts = (products) => {
   return {
     type: ActionTypes.SET_PRODUCTS,
